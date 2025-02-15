0080f3fe9032dbc2e7ac8765565ddef407b50143
akshat498 <akshatdhukia23@gmail.com>
2025-02-15 14:39:16 +0530

    final commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,15 +6,15 @@ cardContent.forEach((card) => {
     cardElement.classList.add('col-12', 'col-md-6', 'col-xl-3', 'mb-4');
 
     cardElement.innerHTML = `
-        <div class="card overflow-hidden">
+        <div class="card overflow-hidden border-0">
             <img src="${card.image}" class="card-img-top w-100" alt="${card.title}">
             <div class="card-body p-0">
                 <h5 class="card-title work-sans-font text-black fw-medium fs-md lh-24 pt-24 mb-0">${card.title}</h5>
                 <div class="gap-8 pt-12 align-items-center ">
-                 <a class="svg-container position-absolute icons d-flex justify-content-center align-items-center">${card.svg_one}</a>
-                  <a class="svg-container-2 position-absolute icons d-flex justify-content-center align-items-center">${card.svg_two}</a>
-                       <a class="svg-container-3 position-absolute icons d-flex justify-content-center align-items-center">${card.svg_three}</a>
-                             <a class="svg-container-4 position-absolute icons d-flex justify-content-center align-items-center">${card.svg_four}</a>
+                 <a class="svg-container position-absolute icons d-flex justify-content-center align-items-center" href="">${card.svg_one}</a>
+                  <a class="svg-container-2 position-absolute icons d-flex justify-content-center align-items-center" href="">${card.svg_two}</a>
+                       <a class="svg-container-3 position-absolute icons d-flex justify-content-center align-items-center" href="">${card.svg_three}</a>
+                             <a class="svg-container-4 position-absolute icons d-flex justify-content-center align-items-center" href="">${card.svg_four}</a>
                 </div>
                 <p class="date fs-sm work-sans-font lh-24 fc-grey text-center fw-normal mb-3">${card.description}</p>
             </div>
